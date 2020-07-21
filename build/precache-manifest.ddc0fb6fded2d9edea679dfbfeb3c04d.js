9f8d27266ab16338615055c250776f9149b89e55
Mahendra <mahendra.aderu@6dtech.co.in>
2020-07-21 18:38:51 +0530

    all changes

---
+++ build/precache-manifest.ddc0fb6fded2d9edea679dfbfeb3c04d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612d1d932e61b2daeaa4fb09aeca2671",
+    "url": "/ReactJs/index.html"
+  },
+  {
+    "revision": "a1d29c4c9dc3c68f5fb4",
+    "url": "/ReactJs/static/css/main.3e708055.chunk.css"
+  },
+  {
+    "revision": "c3deb36ea35ce28b0578",
+    "url": "/ReactJs/static/js/2.efea42fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJs/static/js/2.efea42fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d29c4c9dc3c68f5fb4",
+    "url": "/ReactJs/static/js/main.66dddb32.chunk.js"
+  },
+  {
+    "revision": "92cafe51c61d8b238ab9",
+    "url": "/ReactJs/static/js/runtime-main.7778bd71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJs/static/media/logo.5d5d9eef.svg"
+  }
+]);
